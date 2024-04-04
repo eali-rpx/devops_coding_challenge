e56a67bf35725653f9cd9226338191759ee5b1f3
eali-rpx <50559744+eali-rpx@users.noreply.github.com>
2024-04-04 17:48:56 -0400

    lfunc

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 import yaml
 
+def get_yaml_data(filename):
+    with open(filename, "r") as f:
+        data = yaml.safe_load(f)
+    return data
+
 def lambda_handler(event, context):
-    # Read YAML file
-    with open('data/ebbcarbon.yaml', 'r') as file:
-        yaml_content = yaml.safe_load(file)
-    
-    # Prepare response
-    response = {
-        'statusCode': 200,
-        'body': yaml.dump(yaml_content),
-        'headers': {
-            'Content-Type': 'text/yaml'
+    if event['httpMethod'] == 'GET':
+        # Get the data from the YAML file
+        data = get_yaml_data("data/ebbcarbon.yaml")
+        
+        # Extract the "resources" part
+        resources = data.get("resources")
+        
+        # Return response
+        return {
+            "statusCode": 200,
+            "body": yaml.dump(resources)
         }
-    }
-    
-    return response
+    else:
+        return {
+            "statusCode": 405,
+            "body": "Method Not Allowed"
+        }

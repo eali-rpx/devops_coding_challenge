e62beccd25af87190725423c895da649b5635fac
eali-rpx <50559744+eali-rpx@users.noreply.github.com>
2024-04-05 18:15:30 -0400

    +

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ COPY --from=public.ecr.aws/awsguru/aws-lambda-adapter:0.8.1 /lambda-adapter /opt
 COPY app.py requirements.txt ./
 COPY . .
 RUN python3 -m pip install --no-cache-dir -r requirements.txt
-# CMD ["gunicorn", "-b=:8080", "-w=1", "app:app"]
-CMD ["app.lambda_handler"]
+# CMD ["gunicorn", "-b=:8080", "-w=1", "app:app", "app.lambda_handler"]
+CMD ["app.handler"]
--- app.py
+++ app.py
@@ -1,23 +1,10 @@
-import yaml
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-def get_yaml_data(filename):
-    with open(filename, "r") as f:
-        data = yaml.safe_load(f)
-    return data
-
 @app.route('/api/resources', methods=['GET'])
 def get_resources():
-    # Get the data from the YAML file
-    data = get_yaml_data("data/ebbcarbon.yaml")
-    
-    # Extract the "resources" part
-    resources = data.get("resources")
-    
-    # Return response
-    return jsonify(resources), 200
+    return "Hi EBBCARBOON", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
